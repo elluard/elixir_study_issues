8621577b4e90c6d9b6c7476fb00df9c7c4cf7b8f
elluard <elluard@me.com>
2022-04-16 16:46:31 +0900

    intial commit

---
+++ test/cli_test.exs
@@ -0,0 +1,26 @@
+defmodule CliTest do
+  use ExUnit.Case
+  doctest Issues
+
+  import Issues.CLI, only: [parse_args: 1]
+
+  test "-h 나 --help가 옵션으로 파싱되면 :help 가 반환된다." do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "값을 3개 전달하면, 값 3개가 반환된다." do
+    assert parse_args(["user", "project", "99"]) == { "user","project", 99 }
+  end
+
+  test "값을 2개 전달하면 개수에 기본값을 사용한다." do
+    assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+
+  test "OptionParser 테스트" do
+    # argv = ["user", "project", "99"]
+    argv = ["--limit", "--debug"]
+    a = OptionParser.parse(argv, switches: [limit: :boolean, help: :boolean, debug: :boolean], aliases: [ h: :help ])
+    IO.puts("complete")
+  end
+end
